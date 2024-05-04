9984a21811f1a8f8aa082e96a94a6cba683e0f80
Keith Austin Jr <k.aus2029@gmail.com>
2024-05-04 12:56:59 -0400

    added REST API calls in server file

--- fg-2.0/src/models/server.js
+++ fg-2.0/src/models/server.js
@@ -4,80 +4,78 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('../db.config');
 const app = express();
-
-
-// ALL SQL calls happen here
+const userModel = require('./user/userModel');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+});
+
+// REST API stuff
+
 // routes
 
 // create a user
-app.post("/signup", async(req, res) => {
-    try {
-        const { firstName } = req.body;
-        const { lastName } = req.body;
-        const { email } = req.body;
-        const { username } = req.body;
-        const { pass } = req.body;
-        const newUser = await pool.query("INSERT INTO Users (firstName, lastName, email, username, pass) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
-            [firstName, lastName, email, username, pass]
-        );
-        res.json(newUser);
-    } catch (error) {
-        console.error(error.message);
-    }
+app.post("/signup", async (req, res) => {
+    userModel.createUser(req.body)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
 })
 
 // get all users
-app.get("/login", async(req, res) => {
-    try {
-        const allUsers = await pool.query("SELECT * FROM Users");
-        res.json(allUsers.rows);
-    } catch (error) {
-        console.error(error.message);
-    }
-})
+// app.get("/viewFriends", async (req, res) => {
+//     userModel.getUsers()
+//     .then(response => {
+//         res.status(200).send(response)
+//     })
+//     .catch(error => {
+//         res.status(500).send(error);
+//     })
+// })
 
 // get a user
-app.get("/login/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await pool.query("SELECT * FROM Users WHERE userId = $1", [id]);
-        res.json(user.rows[0]);
-    } catch (error) {
-        console.error(error.message);
-    }
-})
+// app.get("/login/:id", async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const user = await pool.query("SELECT * FROM Users WHERE userId = $1", [id]);
+//         res.json(user.rows[0]);
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// })
 
 // update a user
-app.put("/users/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const { firstName } = req.body;
-        const { lastName } = req.body;
-        const updateUser = await pool.query("UPDATE Person SET firstName = $1, lastName = $2 WHERE personId = $3",
-            [firstName, lastName, id]
-        );
-        res.json("User was updated!");
-    } catch (error) {
-        console.error(error.message);
-    }
-})
+app.put("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    userModel.updateUser(id, body)
+        .then((response) => {
+            res.status(200).send(response);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+});
 
 // delete a user
-app.delete("/users/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const deleteUser = await pool.query("DELETE FROM Person WHERE personId = $1", 
-            [id]
-        );
-        res.json("User was deleted!")
-    } catch (error) {
-        console.error(error.message);
-    }
+app.delete("/users/:id", async (req, res) => {
+    userModel.deleteUser(req.params.id)
+    .then(response => {
+        res.status(200).send(response)
+    })
+    .catch(error => {
+        res.status(500).send(error);
+    })
 })
 
 // Port number must be different from vite and postgres default
