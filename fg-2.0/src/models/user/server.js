e48405470b880c0c2247f9e097f792e0b079bcfe
Keith Austin Jr <k.aus2029@gmail.com>
2024-05-04 09:17:01 -0400

    first dev push

--- fg-2.0/src/models/user/server.js
+++ fg-2.0/src/models/user/server.js
@@ -2,9 +2,22 @@
 
 const express = require('express');
 const cors = require('cors');
-const pool = require('../db.config');
 const app = express();
+const process = require("process");
 
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// Creates the database connection
+const pool = new Pool ({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDB,
+    port: process.env.PGPORT,
+    password: process.env.PGPASS,
+})
+
+// module.exports = pool;
 
 // ALL SQL calls happen here
 
