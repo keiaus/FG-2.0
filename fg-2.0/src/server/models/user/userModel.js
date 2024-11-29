5689aeff4055d301d2d1b18b42614cd779d0ae22
Keith <visionprobro@outlook.com>
2024-11-28 21:45:40 -0500

    perplexity ai to the rescue (yuck)

--- fg-2.0/src/server/models/user/userModel.js
+++ fg-2.0/src/server/models/user/userModel.js
@@ -9,7 +9,8 @@ const main = async () => {
     try {
         await mongoose.connect(url);
         await client.connect();
-        await listDatabases(client)
+        await listDatabases(client);
+        await getSampleData(client);
     } catch (oError) {
         console.error(oError)
     }
@@ -25,4 +26,13 @@ const listDatabases = async (client) => {
     databasesList = await client.db().admin().listDatabases();
     console.log("Databases");
     databasesList.databases.forEach(db => console.log(` - ${db.name} `));
+}
+
+const getSampleData = async (client) => {
+    var db = client.db('sample_mflix');
+    const collection = db.collection('users');
+    const cursor = collection.find({});
+    const documents = await cursor.toArray();
+    console.log("Documents: ", documents);
+    
 }
