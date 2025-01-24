0b23e9c9c5d82f526ca7e1833af7a664f8494476
Keith Austin Jr <k.aus2029@gmail.com>
2025-01-23 21:14:49 -0500

    Updates

--- fg-2.0/src/server/server.js
+++ fg-2.0/src/server/server.js
@@ -1,11 +1,19 @@
-const path = require('path');
-console.log("server: ", require('dotenv').config({ path: path.join(__dirname + 'fg-2.0\.env') }));
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+// const path = require('path');
+// console.log("server: ", require('dotenv').config({ path: path.join(__dirname + 'fg-2.0\.env') }));
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
 
-const url = `${process.env.DB_URL}`;
+/**
+ * Fix for MongooseServerSelectionError: Could not connect to any servers in your MongoDB Atlas cluster. 
+ * One common reason is that you’re trying to access the database from an IP that isn’t whitelisted
+ * 
+ * npm uninstall mongoose
+ * npm install mongoose@8.3.4
+ */
+require('dotenv').config();
+const mongoose = require('mongoose');
+const url = `${process.env.DB_URL}`
 
 mongoose.connect(url)
     .then(() => console.log("Connected!"))
@@ -13,14 +21,14 @@ mongoose.connect(url)
         console.log(`Error connecting to db: ${error}`);
     })
 
-const port = process.env.PORT;
+// const port = process.env.PORT;
 
-const api = require('./routes/api');
+// const api = require('./routes/api');
 
-app.use('/', cors(), api);
+// app.use('/', cors(), api);
 
 // app.on('ready', cors(), () => {
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`App is listening on port ${port}`)
+// })
 // }); 
