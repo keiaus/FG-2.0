2ea6e6befdec28044b2d0d925a3fd328ebe516cd
Keith Austin Jr <k.aus2029@gmail.com>
2025-05-11 23:01:17 -0400

    Login auth fixes

--- fg-2.0/src/client/pages/login/LoginForm.jsx
+++ fg-2.0/src/client/pages/login/LoginForm.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const loginUser = async (credentials) => {
     console.log("credentials: ", credentials);
-    
+
     return fetch('/api/login', {
         method: 'POST',
         headers: {
@@ -14,14 +14,14 @@ const loginUser = async (credentials) => {
         },
         body: JSON.stringify(credentials)
     })
-    .then(data => data.json())
+        .then(data => data.json())
 }
 
-const LoginForm = ({setToken}) => {
+const LoginForm = ({ setToken }) => {
 
     const [username, setUsername] = useState();
     const [pass, setPass] = useState();
-    
+
     const onSubmitForm = async (event) => {
         event.preventDefault();
 
@@ -37,28 +37,35 @@ const LoginForm = ({setToken}) => {
                 }
                 axios.post("/api/login", {
                     method: "POST",
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify(body)
+                    headers: { "Content-Type": "application/json" },
+                    body: body
                 })
-                .then((response) => {
-                    console.log(response);
-                })
-                .catch((error) => {
-                    console.error(error);
-                })
-                // alert("You're logged in as");
+                    .then(async (response) => {
+                        /**
+                         * TODO: Assign the token to the user after a successful login
+                         */
+                        // if (response.status === 200) {
+                        //     let token = null;
+                        //     console.log("ASSIGNING TOKEN TO USER");
+
+                        //     token = await loginUser({
+                        //         username,
+                        //         pass
+                        //     });
+
+                        //     setToken(token);
+                        // }
+                        console.log(response);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    })
             } catch (error) {
                 console.error(error.message);
             }
         }
 
-        // const token = await loginUser({
-        //     username,
-        //     pass
-        // });
-        
-        // setToken(token);
-        
+
     }
 
     return (
@@ -69,13 +76,13 @@ const LoginForm = ({setToken}) => {
             </div>
             <div id="lform-div">
                 <h1 id="login-h1">Log In</h1>
-                <form onSubmit={ onSubmitForm }>
+                <form onSubmit={onSubmitForm}>
                     <label htmlFor="username">Username: </label><br />
                     <input type="text" id="username" value={username} onChange={event => setUsername(event.target.value)}></input><br />
                     <br />
                     <label htmlFor="pass">Password: </label><br />
                     <input type="password" id="pass" value={pass} onChange={event => setPass(event.target.value)}></input><br />
-                    <br/>
+                    <br />
                     <input type="submit" id="submit" value={"Log In"}></input>
                 </form>
             </div>
