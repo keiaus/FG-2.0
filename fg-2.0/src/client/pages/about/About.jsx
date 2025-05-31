957a05f454e81460008aa50069c49a498af65a91
Jennette McCurdy <jenny@Jennettes-Mini.lan>
2025-05-31 04:25:48 -0400

    test

--- fg-2.0/src/client/pages/about/About.jsx
+++ fg-2.0/src/client/pages/about/About.jsx
@@ -12,7 +12,7 @@ const AboutPage = () => {
                     <h1 id="about-h1">What is FG-2.0?</h1>
                 </header>
                 <p id="about-p">FG-2.0 is a web application designed to make traveling with large groups of people<br /> less of a headache.
-                    Instead of having to reach out to a group chat for a date range, <br />FG-2.0 encourages group planning through a calendar.
+                    Instead of having to reach out to a group chat for a date range, <br />FG-2.0 encourages group planning through a calendar..
                 </p>
                 <Footer />
             </div>
