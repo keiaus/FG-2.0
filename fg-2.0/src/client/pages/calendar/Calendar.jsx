0f77d70fa19df468f8547263764b6a5697771317
keiaus <k.aus2029@gmail.com>
2025-10-24 22:56:35 -0400

    Starting group impl

--- fg-2.0/src/client/pages/calendar/Calendar.jsx
+++ fg-2.0/src/client/pages/calendar/Calendar.jsx
@@ -5,7 +5,7 @@ import 'react-calendar/dist/Calendar.css';
 import { useEffect, useState } from "react";
 
 /**
- * TODO: Add buttons for group CRUD actions (this should be on the right side of the calendar)
+ * TODO: Place handle hooks in body (these are just committed ideas)
  * @returns CalendarPage screen
  */
 
@@ -13,7 +13,7 @@ const CalendarPage = () => {
     const [userId, setUserId] = useState();
     const [tokenId, setTokenId] = useState();
     const [dateRange, setDateRange] = useState(new Date());
-    const [newGroup, setNewGroup] = useState({});
+    const [newGroup, setNewGroup] = useState(false);
     const [leaveGroup, setLeaveGroup] = useState(false);
     const [joinGroup, setJoinGroup] = useState(false);
 
@@ -35,6 +35,36 @@ const CalendarPage = () => {
 
     else {
         return alert("Missing username");
+    }
+
+    const handleCreate = async (event) => {
+        event.preventDefault();
+
+        useEffect(() => {
+            setNewGroup(true);
+        });
+
+        alert("Create new group clicked");
+    }
+
+    const handleJoin = async (event) => {
+        event.preventDefault();
+
+        useEffect(() => {
+            setJoinGroup(true);
+        });
+
+        alert("Join group clicked");
+    }
+
+    const handleLeave = async (event) => {
+        event.preventDefault();
+
+        useEffect(() => {
+            setLeaveGroup(true);
+        });
+
+        alert("Leave group clicked");
     }
 
     const onSubmitForm = async (event) => {
@@ -63,39 +93,61 @@ const CalendarPage = () => {
         }
     }
 
-    return (
-        <>
-            <div>
-                <title>Calendar | FG-2.0</title>
-                {/* <Layout /> */}
-                <header>
-                    <h1 id="calendar-title">Calendar</h1>
-                </header>
-                <div id="calendar-div">
-                    <Calendar onChange={setDateRange} value={dateRange} selectRange={true} />
-                    <br />
-                    {dateRange.length > 0 ? (
-                        <p className="text-container">
-                            <span className="bold">Start:</span>{' '}
-                            {dateRange[0].toDateString()}
-                            &nbsp;|&nbsp;
-                            <span className="bold">End:</span> {dateRange[1].toDateString()}
-                        </p>
-                    ) : (
-                        <p className="text-container">
-                            <span className="bold">Today's Date:</span>{' '}
-                            {dateRange.toDateString()}
-                        </p>
-                    )}
+    if (newGroup || joinGroup || leaveGroup) {
+        return (
+            <>
+                <div>
+                    <title>Calendar | FG-2.0</title>
+                    {/* <Layout /> */}
+                    <header>
+                        <h1 id="calendar-title">Calendar</h1>
+                    </header>
+                    <div id="calendar-div">
+                        <Calendar onChange={setDateRange} value={dateRange} selectRange={true} />
+                        <br />
+                        {dateRange.length > 0 ? (
+                            <p className="text-container">
+                                <span className="bold">Start:</span>{' '}
+                                {dateRange[0].toDateString()}
+                                &nbsp;|&nbsp;
+                                <span className="bold">End:</span> {dateRange[1].toDateString()}
+                            </p>
+                        ) : (
+                            <p className="text-container">
+                                <span className="bold">Today's Date:</span>{' '}
+                                {dateRange.toDateString()}
+                            </p>
+                        )}
 
-                    <form onSubmit={onSubmitForm}>
-                        <input type="submit" id="calendar-submit" value={"Confirm"} />
-                    </form>
+                        <form onSubmit={onSubmitForm}>
+                            <input type="submit" id="calendar-submit" value={"Confirm"} />
+                        </form>
 
+                    </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
+
+    else {
+        return (
+            <>
+                <div id="group-div">
+                    <div id="create-div">
+                        <button id="create-grp-btn" onClick={handleCreate}> Create Group </button>
+                    </div>
+                    <div id="join-div">
+                        <button id="join-grp-btn" onClick={handleJoin}> Join Group </button>
+                    </div>
+                    <div id="leave-div">
+                        <button id="leave-grp-btn" onClick={handleLeave}> Leave Group </button>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+
 }
 
 export default CalendarPage;
